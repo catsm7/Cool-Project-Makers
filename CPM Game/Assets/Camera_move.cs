458ec1682dd0b816b7dc5650c390a8916a2a4123
KnickKnaack <nicholasgrijalva01@gmail.com>
2022-02-09 16:00:00 -0700

    prototype?

--- CPM Game/Assets/Camera_move.cs
+++ CPM Game/Assets/Camera_move.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class Camera_move : MonoBehaviour
 {
+
+    float pos1x, pos2x, pos1y, pos2y;
+
     // Start is called before the first frame update
     void Start()
     {
+        pos1x = pos1y = pos2y = 0;
+        pos2x = 18;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(";")) {
+            transform.position = new Vector3(pos1x, pos1y, -3);
+            Camera.main.backgroundColor = new Color(193f/255,228f/255,112f/255);
+        }
+
+        else if (Input.GetKey("'")) {
+            transform.position = new Vector3(pos2x, pos2y, -3);
+            Camera.main.backgroundColor = new Color(229f/255, 152f/255, 230f/255);
+        } 
     }
 }
